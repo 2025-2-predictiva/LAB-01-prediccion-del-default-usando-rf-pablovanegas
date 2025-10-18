6340edef6d75cc30405cf73f1d16a393db83541f
pablovanegas <juvanegas@unal.edu.co>
2025-10-18 13:55:46 -0500

    ya pues xd

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -44,7 +44,7 @@ METRICS = [
     {
         "type": "cm_matrix",
         "dataset": "test",
-        "true_0": {"predicted_0": 6070, "predicted_1": None},
+        "true_0": {"predicted_0": 5000, "predicted_1": None},
         "true_1": {"predicted_0": None, "predicted_1": 760},
     },
 ]
